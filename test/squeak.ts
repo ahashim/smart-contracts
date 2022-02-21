f8429b82bae407f633e2e6b458d12073cc0e8975
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-02-21 17:25:43 -0500

    Updating long squeak test string to remain within a sensible line limit

--- test/squeak.ts
+++ test/squeak.ts
@@ -42,8 +42,10 @@ describe("Squeaks", () => {
   });
 
   it("does not post a squeak that's too long", async () => {
-    const longSqueak =
-      "Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life...";
+    const longSqueak = `Did you ever hear the tragedy of Darth Plagueis The Wise?
+      I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend.
+      Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could
+      use the Force to influence the midichlorians to create life...`;
 
     // assertions
     await expect(contract.postSqueak(longSqueak)).to.be.revertedWith(
