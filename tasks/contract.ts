2fe01695534ef49215ed61e68bc73af5a494472d
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-04-22 10:08:12 -0400

    refactor: Simplifying contract tasks

--- tasks/contract.ts
+++ tasks/contract.ts
@@ -1,69 +1,22 @@
-import { subtask, task } from 'hardhat/config';
-import { faker } from '@faker-js/faker';
+import { task } from 'hardhat/config';
 import { CONTRACT_INITIALIZER, CONTRACT_NAME } from '../constants';
 
 // types
-import type { Contract, ContractTransaction } from 'ethers';
-import type { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
-
-// default values
-const NUMBER_OF_SIGNERS = 20;
-const NUMBER_OF_ACCOUNTS = 3;
-
-subtask(
-  'createAccount',
-  'Creates a single Critter account',
-  async ({
-    contract,
-    signer,
-  }: {
-    contract: Contract;
-    signer: SignerWithAddress;
-  }) => {
-    // create account tx
-    const createAccountTx: ContractTransaction = await contract
-      .connect(signer)
-      .createAccount(faker.name.firstName());
-    await createAccountTx.wait();
-  }
-);
-
-subtask(
-  'deployContract',
-  'Deploys upgradeable contracts via a proxy',
-  async (_, { ethers, upgrades }) => {
-    // deploy contract
-    const factory = await ethers.getContractFactory(CONTRACT_NAME);
-    const contract = await upgrades.deployProxy(factory, CONTRACT_INITIALIZER);
-
-    return contract;
-  }
-);
-
-subtask(
-  'getSigners',
-  'Gets an array of signer accounts of a specified amount',
-  async ({ amount = NUMBER_OF_SIGNERS }: { amount: number }, { ethers }) => {
-    const signers: Array<SignerWithAddress> = await ethers.getSigners();
-
-    return amount === NUMBER_OF_SIGNERS ? signers : signers.slice(0, amount);
-  }
-);
+import type { Contract, ContractFactory } from 'ethers';
 
 task(
-  'initialize',
-  'Deploys contracts and sets up 1 owner + 2 regular accounts',
-  async ({ numberOfAccounts = NUMBER_OF_ACCOUNTS }, { run }) => {
-    const contract: Contract = await run('deployContract');
-    const signers: Array<SignerWithAddress> = await run('getSigners', {
-      amount: numberOfAccounts,
-    });
+  'deployContract',
+  'Deploys upgradeable contracts via a proxy from owner account',
+  async (_, { ethers, upgrades }) => {
+    // deploy contract
+    const factory: ContractFactory = await ethers.getContractFactory(
+      CONTRACT_NAME
+    );
+    const contract: Contract = await upgrades.deployProxy(
+      factory,
+      CONTRACT_INITIALIZER
+    );
 
-    signers.forEach(async (signer) => {
-      await run('createAccount', { contract, signer });
-    });
-
-    // return contract + signers
-    return [contract, signers];
+    return contract;
   }
 );
