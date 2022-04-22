59a8aca7db2a826569387ce200297c9d9a094e46
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-04-22 10:07:47 -0400

    feat: Addign role ids to constants file

--- constants.ts
+++ constants.ts
@@ -1,14 +1,16 @@
 // network
-export const HARDHAT_NETWORK_ID = 31337;
 export const BLOCK_CONFIRMATION_THRESHOLD = 6;
-
-// account
-export const USERNAME = 'a-rock';
 
 // contract
 export const BASE_TOKEN_URI = 'https://critter.fyi/token/';
 export const CONTRACT_NAME = 'Critter';
 export const SYMBOL = 'CRTTR';
+
+// role ID's
+export const MINTER_ROLE = 'MINTER_ROLE';
+export const PAUSER_ROLE = 'PAUSER_ROLE';
+export const TREASURER_ROLE = 'TREASURER_ROLE';
+export const UPGRADER_ROLE = 'UPGRADER_ROLE';
 
 // fees (in wei)
 export const FEE_DELETION = 3_228_041_806_953; // about a penny per block
