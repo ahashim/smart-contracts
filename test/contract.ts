b7e707445b93dac2e2a22e73dc7e57ddffe2d74c
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-04-22 10:09:21 -0400

    refactor: Integrating contract events tests into previously run tests

--- test/contract.ts
+++ test/contract.ts
@@ -1,36 +1,31 @@
 // libraries
 import { expect } from 'chai';
-import { ethers, upgrades } from 'hardhat';
-import { CONTRACT_INITIALIZER, USERNAME } from '../constants';
+import { ethers, run } from 'hardhat';
+import {
+  MINTER_ROLE,
+  PAUSER_ROLE,
+  TREASURER_ROLE,
+  UPGRADER_ROLE,
+} from '../constants';
 
 // types
-import type { Contract, ContractFactory } from 'ethers';
+import type { Contract } from 'ethers';
 import type { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 
-describe('Contract', () => {
-  // contract
+describe.only('Contract', () => {
   let contract: Contract;
-  let factory: ContractFactory;
-
-  // users
   let owner: SignerWithAddress;
   let ahmed: SignerWithAddress;
 
-  // upgrade variables
-  const upgradeAddress = ethers.utils.getAddress(
-    '0x70997970c51812dc3a010c7d01b50e0d17dc79c8'
-  );
+  // role ID's
+  const minterRole: string = ethers.utils.id(MINTER_ROLE);
+  const pauserRole: string = ethers.utils.id(PAUSER_ROLE);
+  const treasurerRole: string = ethers.utils.id(TREASURER_ROLE);
+  const upgraderRole: string = ethers.utils.id(UPGRADER_ROLE);
 
   beforeEach(async () => {
+    contract = await run('deployContract');
     [owner, ahmed] = await ethers.getSigners();
-    factory = await ethers.getContractFactory('Critter');
-
-    // deploy upgradeable contract
-    contract = await upgrades.deployProxy(factory, CONTRACT_INITIALIZER);
-
-    // create an owner account
-    const createAccountTx = await contract.createAccount(USERNAME);
-    await createAccountTx.wait();
   });
 
   describe('interfaces', () => {
@@ -52,17 +47,29 @@ describe('Contract', () => {
   });
 
   describe('state', () => {
-    it('can be paused & unpaused by a user with the PAUSER_ROLE', async () => {
-      // pause the contract from owner account
-      const pauseContractTx = await contract.pause();
-      await pauseContractTx.wait();
+    it('grants the contract owner all available roles after deployment', async () => {
+      [minterRole, pauserRole, treasurerRole, upgraderRole].forEach(
+        async (role) => {
+          // assert owner has all roles associated with their account
+          expect(await contract.getRoleMemberCount(role)).to.equal(1);
+          expect(await contract.getRoleMember(role, 0)).to.equal(
+            owner.address
+          );
+        }
+      );
+    });
 
+    it('can be paused & unpaused by PAUSER_ROLE', async () => {
+      // pause the contract from owner account
+      expect(await contract.connect(owner).pause())
+        .to.emit(contract, 'Paused')
+        .withArgs(owner.address);
       expect(await contract.paused()).to.equal(true);
 
       // unpause the contract from the same account
-      const unpauseContractTx = await contract.unpause();
-      await unpauseContractTx.wait();
-
+      expect(await contract.connect(owner).unpause())
+        .to.emit(contract, 'Unpaused')
+        .withArgs(owner.address);
       expect(await contract.paused()).to.equal(false);
     });
 
@@ -78,34 +85,16 @@ describe('Contract', () => {
 
   describe('upgradeable', () => {
     it('reverts when a user without an UPGRADER_ROLE tries to upgrade', async () => {
+      // upgrade variables
+      const upgradeAddress = ethers.utils.getAddress(
+        '0x70997970c51812dc3a010c7d01b50e0d17dc79c8'
+      );
       await expect(
         // ahmed trying to upgrade the contract w/o an UPGRADER_ROLE
         contract.connect(ahmed).upgradeTo(upgradeAddress)
       ).to.be.revertedWith(
         'AccessControl: account 0x70997970c51812dc3a010c7d01b50e0d17dc79c8 is missing role 0x189ab7a9244df0848122154315af71fe140f3db0fe014031783b0946b8c9d2e3'
       );
-    });
-  });
-
-  describe('events', () => {
-    it('emits a Paused event', async () => {
-      const eventName = 'Paused';
-
-      await expect(contract.pause())
-        .to.emit(contract, eventName)
-        .withArgs(owner.address);
-    });
-
-    it('emits an Unpaused event', async () => {
-      const eventName = 'Unpaused';
-
-      // first pause contract
-      const pauseContractTx = await contract.pause();
-      await pauseContractTx.wait();
-
-      await expect(contract.unpause())
-        .to.emit(contract, eventName)
-        .withArgs(owner.address);
     });
   });
 });
