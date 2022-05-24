9e4a2e8edf97ebb3e43bbebb09a210a809889e68
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-05-24 11:12:02 -0400

    fix: Updating prepare script to not fail on a fresh clone due to typechain-types not being found

--- tasks/project.ts
+++ tasks/project.ts
@@ -4,10 +4,6 @@ task(
   'prepare',
   'Compiles the latest contracts, generates a contract size report & a test coverage report',
   async function (_, { run }) {
-    // compile contracts
-    await run('compile');
-    console.log('\n');
-
     // contract sizing report
     console.log('\x1b[1m%s', 'Contract Size'); // making the title bold
     console.log('%s\x1b[0m', '============='); // reset formatting after separator
