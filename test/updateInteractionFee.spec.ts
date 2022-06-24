531f9704cb2f6e6273246670740b2d590a583b30
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-06-24 07:46:49 -0400

    test: Adding more test cases for `updateInteractionFee()`

--- test/updateInteractionFee.spec.ts
+++ test/updateInteractionFee.spec.ts
@@ -5,24 +5,30 @@ import {
   CONTRACT_INITIALIZER,
   INTERACTION,
   TREASURER_ROLE,
+  PLATFORM_FEE,
 } from '../constants';
 
 // types
-import type { Wallet } from 'ethers';
+import {
+  BigNumber,
+  ContractReceipt,
+  ContractTransaction,
+  Event,
+  Wallet,
+} from 'ethers';
+import type { Result } from '@ethersproject/abi';
 import type { Critter } from '../typechain-types/contracts';
 
 describe('updateInteractionFee', () => {
   let critter: Critter;
   let loadFixture: ReturnType<typeof waffle.createFixtureLoader>;
-  let owner: Wallet, ahmed: Wallet;
+  let owner: Wallet, ahmed: Wallet, barbie: Wallet;
+  let squeakId: BigNumber, updatedFee: BigNumber;
 
   before('create fixture loader', async () => {
-    [owner, ahmed] = await (ethers as any).getSigners();
-    loadFixture = waffle.createFixtureLoader([owner, ahmed]);
+    [owner, ahmed, barbie] = await (ethers as any).getSigners();
+    loadFixture = waffle.createFixtureLoader([owner, ahmed, barbie]);
   });
-
-  // test variables
-  const updatedFee = ethers.utils.parseEther('0.000025');
 
   const updateInteractionFeeFixture = async () => {
     const factory = await ethers.getContractFactory(CONTRACT_NAME);
@@ -35,21 +41,30 @@ describe('updateInteractionFee', () => {
       .connect(owner)
       .grantRole(ethers.utils.id(TREASURER_ROLE), ahmed.address);
 
-    // ahmed updates the interaction fee for "like"
+    // ahmed increases the interaction fee for "like" (note: critter account not
+    // required to update contract fees)
+    const updatedFee = ethers.utils.parseEther('0.0001');
     await critter.updateInteractionFee(INTERACTION.Like, updatedFee);
 
-    return {
-      critter,
-      dislikeFee: await critter.getInteractionFee(INTERACTION.Dislike),
-    };
+    // barbie creates an account & posts a squeak
+    await critter.connect(barbie).createAccount('barbie');
+    const tx = (await critter
+      .connect(barbie)
+      .createSqueak('hello blockchain!')) as ContractTransaction;
+    const receipt = (await tx.wait()) as ContractReceipt;
+    const event = receipt.events!.find(
+      (event: Event) => event.event === 'SqueakCreated'
+    );
+    ({ tokenId: squeakId } = event!.args as Result);
+
+    return { critter, squeakId, updatedFee };
   };
 
-  beforeEach(
-    'deploy test contract, ahmed creates a squeak which is interacted with',
-    async () => {
-      ({ critter } = await loadFixture(updateInteractionFeeFixture));
-    }
-  );
+  beforeEach('deploy test contract, ahmed updates the price', async () => {
+    ({ critter, squeakId, updatedFee } = await loadFixture(
+      updateInteractionFeeFixture
+    ));
+  });
 
   it('allows TREASURER_ROLE to update an interaction fee', async () => {
     expect(await critter.getInteractionFee(INTERACTION.Like)).to.eq(
@@ -59,5 +74,18 @@ describe('updateInteractionFee', () => {
 
   it('reverts when trying to update an invalid interaction', async () => {
     await expect(critter.updateInteractionFee(420, updatedFee)).to.be.reverted;
+  });
+
+  it('reverts when someone other than the TREASURER_ROLE tries to update an interaction fee', async () => {
+    await expect(critter.connect(barbie).updateInteractionFee(420, updatedFee))
+      .to.be.reverted;
+  });
+
+  it('reverts when interacting with the original platform fee', async () => {
+    await expect(
+      critter
+        .connect(barbie)
+        .interact(squeakId, INTERACTION.Like, { value: PLATFORM_FEE })
+    ).to.be.reverted;
   });
 });
