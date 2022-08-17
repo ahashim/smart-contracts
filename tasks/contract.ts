ff1d44a81f4158b268c6ef9bf158d77c7edb2a31
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-08-17 15:44:54 -0400

    refactor: removing unused contract tasks & types

--- tasks/contract.ts
+++ tasks/contract.ts
@@ -1,16 +1,11 @@
 import { task } from 'hardhat/config';
 
 // constants
-import {
-  CONTRACT_INITIALIZER,
-  CONTRACT_NAME,
-  PLATFORM_FEE,
-} from '../constants';
+import { CONTRACT_INITIALIZER, CONTRACT_NAME } from '../constants';
 
 // types
 import type { Contract, ContractFactory, ContractTransaction } from 'ethers';
 import type { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
-import type { TransactionReceipt } from '@ethersproject/providers';
 import type { Critter } from '../typechain-types/contracts';
 
 // tasks
@@ -53,140 +48,5 @@ task(
 
     // return event data
     return receipt.events!.find(({ event }) => event === 'SqueakCreated');
-  }
-);
-
-task(
-  'deleteSqueak',
-  'Create & confirm a signed delete squeak transaction',
-  async ({
-    contract,
-    signer,
-    tokenId,
-  }: {
-    contract: Contract;
-    signer: SignerWithAddress;
-    tokenId: number;
-  }): Promise<TransactionReceipt> => {
-    // get delete fee tx (in the current block, so no waiting)
-    const fee = await contract.getDeleteFee(tokenId);
-
-    // delete the token w/ fee amount
-    const tx: ContractTransaction = await contract
-      .connect(signer)
-      .deleteSqueak(tokenId, { value: fee });
-
-    // wait for a confirmation
-    return await tx.wait();
-  }
-);
-
-task(
-  'dislikeSqueak',
-  'Create & confirm a signed dislike squeak transaction',
-  async ({
-    contract,
-    signer,
-    tokenId,
-  }: {
-    contract: Contract;
-    signer: SignerWithAddress;
-    tokenId: number;
-  }): Promise<TransactionReceipt> => {
-    // delete the token w/ fee amount
-    const tx: ContractTransaction = await contract
-      .connect(signer)
-      .dislikeSqueak(tokenId, { value: PLATFORM_FEE });
-
-    // wait for a confirmation
-    return await tx.wait();
-  }
-);
-
-task(
-  'likeSqueak',
-  'Create & confirm a signed like squeak transaction',
-  async ({
-    contract,
-    signer,
-    tokenId,
-  }: {
-    contract: Contract;
-    signer: SignerWithAddress;
-    tokenId: number;
-  }): Promise<TransactionReceipt> => {
-    // delete the token w/ fee amount
-    const tx: ContractTransaction = await contract
-      .connect(signer)
-      .likeSqueak(tokenId, { value: PLATFORM_FEE });
-
-    // wait for a confirmation
-    return await tx.wait();
-  }
-);
-
-task(
-  'updateUsername',
-  'Create & confirm a signed updateUsername transaction',
-  async ({
-    contract,
-    signer,
-    newUsername,
-  }: {
-    contract: Contract;
-    signer: SignerWithAddress;
-    newUsername: string;
-  }): Promise<TransactionReceipt> => {
-    // update username tx
-    const tx: ContractTransaction = await contract
-      .connect(signer)
-      .updateUsername(newUsername);
-
-    // wait for a confirmation
-    return await tx.wait();
-  }
-);
-
-task(
-  'undoLike',
-  'Create & confirm a signed undoLikeSqueak transaction',
-  async ({
-    contract,
-    signer,
-    tokenId,
-  }: {
-    contract: Contract;
-    signer: SignerWithAddress;
-    tokenId: number;
-  }): Promise<TransactionReceipt> => {
-    // undo like tx
-    const tx: ContractTransaction = await contract
-      .connect(signer)
-      .undoLikeSqueak(tokenId);
-
-    // wait for a confirmation
-    return await tx.wait();
-  }
-);
-
-task(
-  'undoDislike',
-  'Create & confirm a signed undoDislikeSqueak transaction',
-  async ({
-    contract,
-    signer,
-    tokenId,
-  }: {
-    contract: Contract;
-    signer: SignerWithAddress;
-    tokenId: number;
-  }): Promise<TransactionReceipt> => {
-    // undo like tx
-    const tx: ContractTransaction = await contract
-      .connect(signer)
-      .undoDislikeSqueak(tokenId);
-
-    // wait for a confirmation
-    return await tx.wait();
   }
 );
