a8212c590a263b1aa16ce393573bc2832fe2487a
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-08-29 16:46:07 -0400

    test: refactoring `supportsInterface.spec` to use hardhat tasks

--- test/supportsInterface.spec.ts
+++ test/supportsInterface.spec.ts
@@ -1,6 +1,5 @@
 import { expect } from 'chai';
-import { ethers, upgrades, waffle } from 'hardhat';
-import { CONTRACT_NAME, CONTRACT_INITIALIZER } from '../constants';
+import { ethers, run, waffle } from 'hardhat';
 
 // types
 import type { Critter } from '../typechain-types/contracts';
@@ -11,23 +10,6 @@ describe('supportsInterface', () => {
   let loadFixture: ReturnType<typeof waffle.createFixtureLoader>;
   let owner: Wallet;
 
-  before('create fixture loader', async () => {
-    [owner] = await (ethers as any).getSigners();
-    loadFixture = waffle.createFixtureLoader([owner]);
-  });
-
-  const supportsInterfaceFixture = async () => {
-    const factory = await ethers.getContractFactory(CONTRACT_NAME);
-    return (await upgrades.deployProxy(
-      factory,
-      CONTRACT_INITIALIZER
-    )) as Critter;
-  };
-
-  beforeEach('load deployed contract fixture', async () => {
-    critter = await loadFixture(supportsInterfaceFixture);
-  });
-
   // tests variables
   const interfaceIds = {
     ERC165: '0x01ffc9a7',
@@ -35,6 +17,17 @@ describe('supportsInterface', () => {
     ERC721Metadata: '0x5b5e139f',
     Other: '0xa2b4c6d8',
   };
+
+  before('create fixture loader', async () => {
+    [owner] = await (ethers as any).getSigners();
+    loadFixture = waffle.createFixtureLoader([owner]);
+  });
+
+  const supportsInterfaceFixture = async () => await run('deploy-contract');
+
+  beforeEach('load deployed contract fixture', async () => {
+    critter = await loadFixture(supportsInterfaceFixture);
+  });
 
   it('supports the ERCI65 interface', async () => {
     expect(await critter.supportsInterface(interfaceIds.ERC165)).to.be.true;
