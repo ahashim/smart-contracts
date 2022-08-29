b95f44313862c3cb4978425fb154c50654df15ab
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-08-29 15:27:09 -0400

    test: refactoring `initialize.spec` to use hardhat tasks

--- test/initialize.spec.ts
+++ test/initialize.spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { ethers, waffle, upgrades } from 'hardhat';
+import { run, waffle } from 'hardhat';
 import {
   BASE_TOKEN_URI,
   CONTRACT_NAME,
@@ -13,36 +13,17 @@ import {
 } from '../constants';
 
 // types
-import type { ContractFactory, Wallet } from 'ethers';
 import type { Critter } from '../typechain-types/contracts';
 
 describe('initialize', () => {
   let critter: Critter;
   let loadFixture: ReturnType<typeof waffle.createFixtureLoader>;
-  let owner: Wallet;
 
   before('create fixture loader', async () => {
-    [owner] = await (ethers as any).getSigners();
-    loadFixture = waffle.createFixtureLoader([owner]);
+    loadFixture = waffle.createFixtureLoader();
   });
 
-  const initializeFixture = async () => {
-    const factory = (await ethers.getContractFactory(
-      CONTRACT_NAME
-    )) as ContractFactory;
-
-    return (await upgrades.deployProxy(factory, [
-      CONTRACT_NAME,
-      CONTRACT_SYMBOL,
-      BASE_TOKEN_URI,
-      PLATFORM_FEE,
-      PLATFORM_TAKE_RATE,
-      SCOUT_POOL_THRESHOLD,
-      VIRALITY_THRESHOLD,
-      SCOUT_BONUS,
-      SCOUT_MAX_LEVEL,
-    ])) as Critter;
-  };
+  const initializeFixture = async () => await run('deploy-contract');
 
   beforeEach('load deployed contract fixture', async () => {
     critter = await loadFixture(initializeFixture);
