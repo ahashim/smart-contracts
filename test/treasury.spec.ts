e16ee96fd73785a9c52914bc7fb2fa8590f51146
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-08-29 17:02:11 -0400

    test: refactoring `treasury.spec` to use hardhat tasks

--- test/treasury.spec.ts
+++ test/treasury.spec.ts
@@ -1,17 +1,9 @@
 import { expect } from 'chai';
-import { ethers, upgrades, waffle } from 'hardhat';
-import { CONTRACT_NAME, CONTRACT_INITIALIZER } from '../constants';
+import { ethers, run, waffle } from 'hardhat';
 import { Interaction } from '../enums';
 
 // types
-import {
-  BigNumber,
-  ContractReceipt,
-  ContractTransaction,
-  Event,
-  Wallet,
-} from 'ethers';
-import type { Result } from '@ethersproject/abi';
+import { BigNumber, Wallet } from 'ethers';
 import type { Critter } from '../typechain-types/contracts';
 
 describe('treasury', () => {
@@ -26,24 +18,21 @@ describe('treasury', () => {
   });
 
   const treasuryFixture = async () => {
-    const factory = await ethers.getContractFactory(CONTRACT_NAME);
-    const critter = (
-      await upgrades.deployProxy(factory, CONTRACT_INITIALIZER)
-    ).connect(ahmed) as Critter;
+    // deploy contract
+    const critter = (await run('deploy-contract')).connect(ahmed);
 
-    // creates accounts
-    await critter.createAccount('ahmed');
-    await critter.connect(barbie).createAccount('barbie');
+    // everybody creates an account
+    await run('create-accounts', {
+      accounts: [ahmed, barbie],
+      contract: critter,
+    });
 
     // ahmed posts a squeak
-    const tx = (await critter.createSqueak(
-      'hello blockchain!'
-    )) as ContractTransaction;
-    const receipt = (await tx.wait()) as ContractReceipt;
-    const event = receipt.events!.find(
-      (event: Event) => event.event === 'SqueakCreated'
-    );
-    ({ tokenId: squeakId } = event!.args as Result);
+    ({ squeakId } = await run('create-squeak', {
+      content: 'hello blockchain!',
+      contract: critter,
+      signer: ahmed,
+    }));
 
     // barbie dislikes it
     dislikeFee = await critter.fees(Interaction.Dislike);
