bd2b25aa23b8e6a8c2381897aaaf5fd1898e3104
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-08-29 13:09:09 -0400

    test: refactoring `getApproved.spec` to use hardhat tasks

--- test/getApproved.spec.ts
+++ test/getApproved.spec.ts
@@ -1,15 +1,9 @@
-import {
-  BigNumber,
-  ContractReceipt,
-  ContractTransaction,
-  Event,
-  Wallet,
-} from 'ethers';
-import { Result } from '@ethersproject/abi';
-import { ethers, upgrades, waffle } from 'hardhat';
+import { ethers, run, waffle } from 'hardhat';
 import { expect } from 'chai';
-import { CONTRACT_NAME, CONTRACT_INITIALIZER } from '../constants';
-import { Critter } from '../typechain-types/contracts';
+
+// types
+import type { Critter } from '../typechain-types/contracts';
+import type { BigNumber, Wallet } from 'ethers';
 
 describe('getApproved', () => {
   let critter: Critter;
@@ -23,21 +17,18 @@ describe('getApproved', () => {
   });
 
   const approveFixture = async () => {
-    const factory = await ethers.getContractFactory(CONTRACT_NAME);
-    const critter = (
-      await upgrades.deployProxy(factory, CONTRACT_INITIALIZER)
-    ).connect(ahmed) as Critter;
+    // deploy contract
+    critter = (await run('deploy-contract')).connect(ahmed);
 
-    // ahmed creates an account & posts a squeak
+    // ahmed creates an account
     await critter.createAccount('ahmed');
-    const tx = (await critter.createSqueak(
-      'hello blockchain!'
-    )) as ContractTransaction;
-    const receipt = (await tx.wait()) as ContractReceipt;
-    const event = receipt.events!.find(
-      (event: Event) => event.event === 'SqueakCreated'
-    );
-    ({ tokenId: squeakId } = event!.args as Result);
+
+    // ahmed posts a squeak
+    ({ squeakId } = await run('create-squeak', {
+      content: 'hello blockchain!',
+      contract: critter,
+      signer: ahmed,
+    }));
 
     // barbie creates an account
     critter.connect(barbie).createAccount('barbie');
@@ -48,12 +39,9 @@ describe('getApproved', () => {
     return { critter, squeakId };
   };
 
-  beforeEach(
-    'load deployed contract fixture, barbie & ahmed create an accounts, ahmed posts a squeak and approves barbie to transfer it',
-    async () => {
-      ({ critter, squeakId } = await loadFixture(approveFixture));
-    }
-  );
+  beforeEach('load deployed contract fixture', async () => {
+    ({ critter, squeakId } = await loadFixture(approveFixture));
+  });
 
   it('returns the account approved to manage a squeak', async () => {
     expect(await critter.getApproved(squeakId)).to.eq(barbie.address);
