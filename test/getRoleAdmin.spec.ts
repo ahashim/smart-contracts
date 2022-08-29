3755c24f89dcbd6c8751132aecfdd2c435394217
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-08-29 14:57:29 -0400

    test: refactoring `getRoleAdmin` to use hardhat tasks

--- test/getRoleAdmin.spec.ts
+++ test/getRoleAdmin.spec.ts
@@ -1,9 +1,7 @@
 import { Wallet } from 'ethers';
-import { ethers, upgrades, waffle } from 'hardhat';
+import { ethers, run, waffle } from 'hardhat';
 import { expect } from 'chai';
 import {
-  CONTRACT_NAME,
-  CONTRACT_INITIALIZER,
   MINTER_ROLE,
   PAUSER_ROLE,
   TREASURER_ROLE,
@@ -17,28 +15,22 @@ describe('getRoleAdmin', () => {
   let owner: Wallet;
   let roleAdmin: string;
 
-  before('create fixture loader', async () => {
-    [owner] = await (ethers as any).getSigners();
-    loadFixture = waffle.createFixtureLoader([owner]);
-  });
-
-  const getRoleAdminFixture = async () => {
-    const factory = await ethers.getContractFactory(CONTRACT_NAME);
-    return (await upgrades.deployProxy(
-      factory,
-      CONTRACT_INITIALIZER
-    )) as Critter;
-  };
-
-  beforeEach('load deployed contract fixture', async () => {
-    critter = await loadFixture(getRoleAdminFixture);
-  });
-
   // tests variables
   const ID_MINTER_ROLE = ethers.utils.id(MINTER_ROLE);
   const ID_PAUSER_ROLE = ethers.utils.id(PAUSER_ROLE);
   const ID_TREASURER_ROLE = ethers.utils.id(TREASURER_ROLE);
   const ID_UPGRADER_ROLE = ethers.utils.id(UPGRADER_ROLE);
+
+  before('create fixture loader', async () => {
+    [owner] = await (ethers as any).getSigners();
+    loadFixture = waffle.createFixtureLoader([owner]);
+  });
+
+  const getRoleAdminFixture = async () => await run('deploy-contract');
+
+  beforeEach('load deployed contract fixture', async () => {
+    critter = await loadFixture(getRoleAdminFixture);
+  });
 
   it('sets the owner account as the role-admin for the MINTER_ROLE', async () => {
     roleAdmin = await critter.getRoleAdmin(ID_MINTER_ROLE);
