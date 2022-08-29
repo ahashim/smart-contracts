4633d1b2dd305d5680a9e86dd25fd261bfb5da35
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-08-29 16:25:06 -0400

    test: refactoring `revokeRole.spec` to use hardhat tasks

--- test/revokeRole.spec.ts
+++ test/revokeRole.spec.ts
@@ -1,10 +1,6 @@
 import { expect } from 'chai';
-import { ethers, waffle, upgrades } from 'hardhat';
-import {
-  CONTRACT_NAME,
-  CONTRACT_INITIALIZER,
-  MINTER_ROLE,
-} from '../constants';
+import { ethers, run, waffle } from 'hardhat';
+import { MINTER_ROLE } from '../constants';
 
 // types
 import { Wallet } from 'ethers';
@@ -15,34 +11,34 @@ describe('revokeRole', () => {
   let loadFixture: ReturnType<typeof waffle.createFixtureLoader>;
   let owner: Wallet, ahmed: Wallet;
 
+  // test variables
+  const ID_MINTER_ROLE = ethers.utils.id(MINTER_ROLE);
+
   before('create fixture loader', async () => {
     [owner, ahmed] = await (ethers as any).getSigners();
     loadFixture = waffle.createFixtureLoader([owner, ahmed]);
   });
 
   const revokeRoleFixture = async () => {
-    const factory = await ethers.getContractFactory(CONTRACT_NAME);
-    const critter = (await upgrades.deployProxy(
-      factory,
-      CONTRACT_INITIALIZER
-    )) as Critter;
-    await critter.createAccount('ahmed');
+    // deploy contract
+    const critter = await run('deploy-contract');
+
+    // everybody creates an account
+    await run('create-accounts', {
+      accounts: [owner, ahmed],
+      contract: critter,
+    });
 
     return critter;
   };
 
-  beforeEach(
-    'load deployed contract fixture, and ahmed creates an account that gives him MINTER_ROLE access',
-    async () => {
-      critter = await loadFixture(revokeRoleFixture);
-    }
-  );
-
-  // test variables
-  const ID_MINTER_ROLE = ethers.utils.id(MINTER_ROLE);
+  beforeEach('load deployed contract fixture', async () => {
+    critter = await loadFixture(revokeRoleFixture);
+  });
 
   it('lets a role-admin revoke a users role', async () => {
     await critter.revokeRole(ID_MINTER_ROLE, ahmed.address);
+
     expect(await critter.hasRole(ID_MINTER_ROLE, ahmed.address)).to.be.false;
   });
 
