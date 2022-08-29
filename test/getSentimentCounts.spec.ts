dba9b7a0d38ef5f329b7bf676a71d64fb39fe892
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-08-29 15:04:46 -0400

    test: refactoring `getSentimentCounts.spec` to use hardhat tasks

--- test/getSentimentCounts.spec.ts
+++ test/getSentimentCounts.spec.ts
@@ -1,17 +1,10 @@
 import { expect } from 'chai';
-import { ethers, upgrades, waffle } from 'hardhat';
-import { CONTRACT_NAME, CONTRACT_INITIALIZER, OVERFLOW } from '../constants';
+import { ethers, run, waffle } from 'hardhat';
+import { OVERFLOW } from '../constants';
 import { Interaction } from '../enums';
 
 // types
-import type {
-  BigNumber,
-  ContractReceipt,
-  ContractTransaction,
-  Event,
-  Wallet,
-} from 'ethers';
-import type { Result } from '@ethersproject/abi';
+import type { BigNumber, Wallet } from 'ethers';
 import type { Critter } from '../typechain-types/contracts';
 import type { SentimentCounts } from '../types';
 
@@ -19,10 +12,8 @@ describe('getSentimentCounts', () => {
   let critter: Critter;
   let loadFixture: ReturnType<typeof waffle.createFixtureLoader>;
   let owner: Wallet, ahmed: Wallet, barbie: Wallet, carlos: Wallet;
-  let receipt: ContractReceipt;
   let sentimentCounts: SentimentCounts;
   let squeakId: BigNumber;
-  let tx: ContractTransaction;
 
   before('create fixture loader', async () => {
     [owner, ahmed, barbie, carlos] = await (ethers as any).getSigners();
@@ -30,37 +21,43 @@ describe('getSentimentCounts', () => {
   });
 
   const getSentimentCountsFixture = async () => {
-    const factory = await ethers.getContractFactory(CONTRACT_NAME);
-    const critter = (
-      await upgrades.deployProxy(factory, CONTRACT_INITIALIZER)
-    ).connect(ahmed) as Critter;
+    critter = (await run('deploy-contract')).connect(ahmed);
 
-    // everybody creates an account
-    [ahmed, barbie, carlos].forEach(async (account, index) => {
-      await critter.connect(account).createAccount(index.toString());
+    // creates accounts
+    await run('create-accounts', {
+      accounts: [ahmed, barbie, carlos],
+      contract: critter,
     });
 
     // ahmed posts a squeak
-    tx = await critter.createSqueak('hello blockchain!');
-    receipt = await tx.wait();
-    const event = receipt.events!.find(
-      (event: Event) => event.event === 'SqueakCreated'
-    );
-    ({ tokenId: squeakId } = event!.args as Result);
+    ({ squeakId } = await run('create-squeak', {
+      content: 'hello blockchain!',
+      contract: critter,
+      signer: ahmed,
+    }));
 
     // ahmed resqueaks it
-    await critter.interact(squeakId, Interaction.Resqueak, {
-      value: await critter.fees(Interaction.Resqueak),
+    await run('interact', {
+      contract: critter,
+      interaction: Interaction.Resqueak,
+      signer: ahmed,
+      squeakId,
     });
 
     // barbie likes it
-    await critter.connect(barbie).interact(squeakId, Interaction.Like, {
-      value: await critter.fees(Interaction.Like),
+    await run('interact', {
+      contract: critter,
+      interaction: Interaction.Like,
+      signer: barbie,
+      squeakId,
     });
 
     // carlos dislikes it
-    await critter.connect(carlos).interact(squeakId, Interaction.Dislike, {
-      value: await critter.fees(Interaction.Dislike),
+    await run('interact', {
+      contract: critter,
+      interaction: Interaction.Dislike,
+      signer: carlos,
+      squeakId,
     });
 
     return {
