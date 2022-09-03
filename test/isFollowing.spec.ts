645ad430dbac03100002188a19c52f2e8902f026
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-09-03 12:00:53 -0400

    test: adding test suite for `isFollowing.spec`

--- test/isFollowing.spec.ts
+++ test/isFollowing.spec.ts
@@ -6,7 +6,7 @@ import type { Wallet } from 'ethers';
 import type { Critter } from '../typechain-types/contracts';
 import { Relation } from '../enums';
 
-describe('isBlocked', () => {
+describe('isFollowing', () => {
   let critter: Critter;
   let loadFixture: ReturnType<typeof waffle.createFixtureLoader>;
   let owner: Wallet, ahmed: Wallet, barbie: Wallet;
@@ -16,7 +16,7 @@ describe('isBlocked', () => {
     loadFixture = waffle.createFixtureLoader([owner, ahmed, barbie]);
   });
 
-  const isBlockedFixture = async () => {
+  const isFollowingFixture = async () => {
     // deploy contract
     critter = (await run('deploy-contract')).connect(ahmed);
 
@@ -26,17 +26,18 @@ describe('isBlocked', () => {
       contract: critter,
     });
 
-    // ahmed blocks barbie
-    await critter.updateRelationship(barbie.address, Relation.Block);
+    // ahmed follows barbie
+    await critter.updateRelationship(barbie.address, Relation.Follow);
 
     return critter;
   };
 
   beforeEach('load deployed contract fixture', async () => {
-    critter = await loadFixture(isBlockedFixture);
+    critter = await loadFixture(isFollowingFixture);
   });
 
-  it('returns true if user one has blocked user two', async () => {
-    expect(await critter.isBlocked(ahmed.address, barbie.address)).to.be.true;
+  it('returns true if user one is following user two', async () => {
+    expect(await critter.isFollowing(ahmed.address, barbie.address)).to.be
+      .true;
   });
 });
