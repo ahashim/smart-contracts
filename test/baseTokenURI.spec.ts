6e6e51cf3a0dc79b955d0cd9e108024d2873e27e
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-10-17 11:25:04 -0400

    test: improving `baseTokenURI` cases & upgrading suite to the hardhat fixture loader

--- test/baseTokenURI.spec.ts
+++ test/baseTokenURI.spec.ts
@@ -1,28 +1,18 @@
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
-import { ethers, run, waffle } from 'hardhat';
+import hardhat from 'hardhat';
 import { BASE_TOKEN_URI } from '../constants';
 
 // types
-import type { Wallet } from 'ethers';
 import type { Critter } from '../typechain-types/contracts';
 
 describe('baseTokenURI', () => {
   let critter: Critter;
-  let loadFixture: ReturnType<typeof waffle.createFixtureLoader>;
-  let owner: Wallet;
 
-  before('create fixture loader', async () => {
-    [owner] = await (ethers as any).getSigners();
-    loadFixture = waffle.createFixtureLoader([owner]);
-  });
-
-  const baseTokenURIFixture = async () => await run('deploy-contract');
-
-  beforeEach('load deployed contract fixture', async () => {
-    critter = await loadFixture(baseTokenURIFixture);
-  });
+  const baseTokenURIFixture = async () => await hardhat.run('deploy-contract');
 
   it('returns the base token URI', async () => {
+    critter = await loadFixture(baseTokenURIFixture);
     expect(await critter.baseTokenURI()).to.eq(BASE_TOKEN_URI);
   });
 });
