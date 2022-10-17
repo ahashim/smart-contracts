5b2c2c47463a0c94b8ab067d67480524bd92e008
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-10-17 12:07:53 -0400

    test: improving `createSqueak.spec` cases & upgrading suite to the hardhat fixture loader

--- test/createSqueak.spec.ts
+++ test/createSqueak.spec.ts
@@ -1,35 +1,34 @@
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
-import { ethers, run, waffle } from 'hardhat';
+import hardhat from 'hardhat';
 import { MODERATOR_ROLE } from '../constants';
 import { Status } from '../enums';
 
 // types
-import type {
-  BigNumber,
-  ContractReceipt,
-  ContractTransaction,
-  Wallet,
-} from 'ethers';
+import type { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import type { BigNumber, ContractReceipt, ContractTransaction } from 'ethers';
 import type { Critter } from '../typechain-types/contracts';
 import type { Squeak } from '../types';
 
 describe('createSqueak', () => {
-  let critter: Critter;
-  let content = 'hello blockchain!';
-  let loadFixture: ReturnType<typeof waffle.createFixtureLoader>;
-  let owner: Wallet, ahmed: Wallet, barbie: Wallet, carlos: Wallet;
-  let receipt: ContractReceipt;
-  let squeak: Squeak;
-  let squeakId: BigNumber;
-  let tx: ContractTransaction;
-
-  before('create fixture loader', async () => {
-    [owner, ahmed, barbie, carlos] = await (ethers as any).getSigners();
-    loadFixture = waffle.createFixtureLoader([owner, ahmed, barbie, carlos]);
-  });
+  const content = 'hello blockchain!';
+  const rawContent = hardhat.ethers.utils.hexlify(
+    hardhat.ethers.utils.toUtf8Bytes(content)
+  );
+  let accountBalance: BigNumber,
+    ahmed: SignerWithAddress,
+    barbie: SignerWithAddress,
+    carlos: SignerWithAddress,
+    critter: Critter,
+    owner: SignerWithAddress,
+    receipt: ContractReceipt,
+    squeak: Squeak,
+    squeakId: BigNumber,
+    tx: ContractTransaction;
 
   const createSqueakFixture = async () => {
-    critter = (await run('deploy-contract')).connect(ahmed);
+    [owner, ahmed, barbie, carlos] = await hardhat.ethers.getSigners();
+    critter = (await hardhat.run('deploy-contract')).connect(ahmed);
 
     // ahmed creates an account
     await critter.createAccount('ahmed');
@@ -37,10 +36,10 @@ describe('createSqueak', () => {
     // contract owner grants ahmed the moderator role
     await critter
       .connect(owner)
-      .grantRole(ethers.utils.id(MODERATOR_ROLE), ahmed.address);
+      .grantRole(hardhat.ethers.utils.id(MODERATOR_ROLE), ahmed.address);
 
     // ahmed creates a squeak
-    ({ receipt, squeakId, tx } = await run('create-squeak', {
+    ({ receipt, squeakId, tx } = await hardhat.run('create-squeak', {
       content,
       contract: critter,
       signer: ahmed,
@@ -53,7 +52,7 @@ describe('createSqueak', () => {
     await critter.updateStatus(barbie.address, Status.Banned);
 
     return {
-      content,
+      accountBalance: await critter.balanceOf(ahmed.address),
       critter,
       receipt,
       squeak: await critter.squeaks(squeakId),
@@ -62,18 +61,20 @@ describe('createSqueak', () => {
   };
 
   beforeEach('load deployed contract fixture', async () => {
-    ({ content, critter, receipt, squeak, tx } = await loadFixture(
+    ({ accountBalance, critter, receipt, squeak, tx } = await loadFixture(
       createSqueakFixture
     ));
   });
 
   it('lets a user create a squeak', async () => {
-    const rawContent = ethers.utils.hexlify(ethers.utils.toUtf8Bytes(content));
-
     expect(squeak.blockNumber).to.eq(receipt.blockNumber);
     expect(squeak.author).to.eq(ahmed.address);
     expect(squeak.owner).to.eq(ahmed.address);
     expect(squeak.content).to.eq(rawContent);
+  });
+
+  it('mints a token to the creators account', async () => {
+    expect(accountBalance).to.eq(1);
   });
 
   it('emits a SqueakCreated event', async () => {
@@ -83,25 +84,30 @@ describe('createSqueak', () => {
   });
 
   it('reverts when the squeak content is empty', async () => {
-    await expect(critter.createSqueak('')).to.be.reverted;
+    await expect(critter.createSqueak('')).to.be.revertedWithCustomError(
+      critter,
+      'SqueakEmpty'
+    );
   });
 
   it('reverts when the squeak content is too long', async () => {
-    const longContent = `Did you ever hear the tragedy of Darth Plagueis The
+    await expect(
+      critter.createSqueak(`Did you ever hear the tragedy of Darth Plagueis The
     Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith
     legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise
-    he could use the Force to influence the midichlorians to create life...`;
-
-    await expect(critter.createSqueak(longContent)).to.be.reverted;
+    he could use the Force to influence the midichlorians to create life...`)
+    ).to.be.revertedWithCustomError(critter, 'SqueakTooLong');
   });
 
   it('reverts when the user does not have an account', async () => {
-    await expect(critter.connect(carlos).createSqueak(content)).to.be.reverted;
+    await expect(
+      critter.connect(carlos).createSqueak(content)
+    ).to.be.revertedWithCustomError(critter, 'InvalidAccount');
   });
 
   it('reverts when the account is not active', async () => {
     await expect(
       critter.connect(barbie).createSqueak('come on barbie, lets go party')
-    ).to.be.reverted;
+    ).to.be.revertedWithCustomError(critter, 'InvalidAccountStatus');
   });
 });
