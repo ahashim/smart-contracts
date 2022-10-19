f95df816fd08e10e970535301663e20241c80691
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-10-19 08:28:12 -0400

    test: improving `isViral.spec` cases & upgrading suite to the hardhat fixture loader

--- test/isViral.spec.ts
+++ test/isViral.spec.ts
@@ -1,38 +1,38 @@
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
-import { ethers, run, waffle } from 'hardhat';
+import hardhat from 'hardhat';
 import { Interaction } from '../enums';
 
 // types
-import type { BigNumber, Wallet } from 'ethers';
+import type { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import type { BigNumber } from 'ethers';
 import type { Critter } from '../typechain-types/contracts';
 
 describe('isViral', () => {
-  let critter: Critter;
-  let squeakId: BigNumber;
-  let loadFixture: ReturnType<typeof waffle.createFixtureLoader>;
-  let owner: Wallet, ahmed: Wallet, barbie: Wallet, carlos: Wallet;
-
-  before('create fixture loader', async () => {
-    [owner, ahmed, barbie, carlos] = await (ethers as any).getSigners();
-    loadFixture = waffle.createFixtureLoader([owner, ahmed, barbie, carlos]);
-  });
+  let ahmed: SignerWithAddress,
+    barbie: SignerWithAddress,
+    carlos: SignerWithAddress,
+    critter: Critter,
+    nonViralSqueakId: BigNumber,
+    viralSqueakId: BigNumber;
 
   const isViralFixture = async () => {
+    [, ahmed, barbie, carlos] = await hardhat.ethers.getSigners();
     // deploy contract with a lower virality threshold
     critter = (
-      await run('deploy-contract', {
+      await hardhat.run('deploy-contract', {
         viralityThreshold: 1,
       })
     ).connect(ahmed);
 
     // creates accounts
-    await run('create-accounts', {
+    await hardhat.run('create-accounts', {
       accounts: [ahmed, barbie, carlos],
       contract: critter,
     });
 
     // ahmed creates a squeak
-    ({ squeakId } = await run('create-squeak', {
+    ({ squeakId: viralSqueakId } = await hardhat.run('create-squeak', {
       content: 'hello blockchain!',
       contract: critter,
       signer: ahmed,
@@ -40,34 +40,49 @@ describe('isViral', () => {
 
     // ahmed & barbie resqueak it
     [ahmed, barbie].forEach(async (signer) => {
-      await run('interact', {
+      await hardhat.run('interact', {
         contract: critter,
         interaction: Interaction.Resqueak,
         signer,
-        squeakId,
+        squeakId: viralSqueakId,
       });
     });
 
     // carlos likes it, and thus makes it eligible for virality
-    await run('interact', {
+    await hardhat.run('interact', {
       contract: critter,
       interaction: Interaction.Like,
       signer: carlos,
-      squeakId,
+      squeakId: viralSqueakId,
     });
 
-    return { critter, squeakId };
+    ({ squeakId: nonViralSqueakId } = await hardhat.run('create-squeak', {
+      content: 'i like turtles',
+      contract: critter,
+      signer: carlos,
+    }));
+
+    return { critter, nonViralSqueakId, viralSqueakId };
   };
 
   beforeEach('load deployed contract fixture', async () => {
-    ({ critter, squeakId } = await loadFixture(isViralFixture));
+    ({ critter, nonViralSqueakId, viralSqueakId } = await loadFixture(
+      isViralFixture
+    ));
   });
 
-  it('returns the viral status of a squeak', async () => {
-    expect(await critter.isViral(squeakId)).to.be.true;
+  it('returns true when a squeak is viral', async () => {
+    expect(await critter.isViral(viralSqueakId)).to.be.true;
+  });
+
+  it('returns false when a squeak is not viral', async () => {
+    expect(await critter.isViral(nonViralSqueakId)).to.be.false;
   });
 
   it('reverts when looking up an unknown squeak', async () => {
-    await expect(critter.isViral(420)).to.be.reverted;
+    await expect(critter.isViral(420)).to.be.revertedWithCustomError(
+      critter,
+      'SqueakDoesNotExist'
+    );
   });
 });
