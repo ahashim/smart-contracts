b09b34bfdcecdab57d2abebeaaaa0c3acced69a2
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-10-19 10:35:07 -0400

    test: improving `symbol.spec` cases & upgrading suite to the hardhat fixture loader

--- test/symbol.spec.ts
+++ test/symbol.spec.ts
@@ -1,26 +1,25 @@
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
-import { run, waffle } from 'hardhat';
+import hardhat from 'hardhat';
 import { CONTRACT_SYMBOL } from '../constants';
 
 // types
 import type { Critter } from '../typechain-types/contracts';
 
 describe('symbol', () => {
-  let critter: Critter;
-  let loadFixture: ReturnType<typeof waffle.createFixtureLoader>;
+  let critter: Critter, symbol: string;
 
-  before(
-    'create fixture loader',
-    async () => (loadFixture = waffle.createFixtureLoader())
-  );
+  const symbolFixture = async () => {
+    critter = await hardhat.run('deploy-contract');
 
-  const symbolFixture = async () => await run('deploy-contract');
+    return await critter.symbol();
+  };
 
   beforeEach('load deployed contract fixture', async () => {
-    critter = await loadFixture(symbolFixture);
+    symbol = await loadFixture(symbolFixture);
   });
 
-  it('returns the contract symbol', async () => {
-    expect(await critter.symbol()).to.eq(CONTRACT_SYMBOL);
+  it('returns the contract symbol', () => {
+    expect(symbol).to.eq(CONTRACT_SYMBOL);
   });
 });
