02543afeac05cb68557aea26f005d3b53d599584
Ahmed Hashim <ahashim@users.noreply.github.com>
2022-12-10 09:04:57 -0500

    fix: only exporting `Critter.sol` contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,6 +15,7 @@ import type { HardhatUserConfig } from 'hardhat/config';
 const config: HardhatUserConfig = {
   abiExporter: {
     clear: true,
+    only: ['Critter.sol'],
   },
   contractSizer: {
     strict: true,
